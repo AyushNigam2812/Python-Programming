dc4059ce0a9d017ce13344ce3799d46effab8624
AyushNigam2812 <mohitnigam2812@gmail.com>
2025-11-28 01:27:27 +0530

    Add files via upload

---
+++ multiple of 7.py
@@ -0,0 +1,6 @@
+# Program to check if a number is a multiple of 7
+num = int(input("Enter a number: "))
+if num % 7 == 0:
+    print(num, "is a multiple of 7")
+else:
+    print(num, "is NOT a multiple of 7")
